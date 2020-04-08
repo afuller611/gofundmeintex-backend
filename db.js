027e6c5e592235935aaafdd70a19caa0f2593834
Andre Fuller <afuller611@gmail.com>
2020-04-08 13:04:40 -0600

    using env variables now

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize')
 const { QueryTypes } = require('sequelize');
+require('dotenv').config()
 
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-const sequelize = new Sequelize("Intex-Two", "jand", "Ernie2020", {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+// const sequelize = new Sequelize("Intex-Two", "jand", "Ernie2020", {
+    host: process.env.DB_SERVER,
 // const sequelize = new Sequelize("shared", "mkshared", "Share0ata", {
 //     host: "http://mkshared.database.windows.net",
     dialect: "mssql",
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require('cors')
 const bodyParser = require("body-parser");
 
 require('dotenv').config()
-console.log(process.env.DB_NAME)
 
 app.use(bodyParser.json());
 
@@ -15,6 +14,6 @@ app.use(cors())
 app.use('/api', apiroutes)
 // app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+app.listen(process.env.PORT || port, () => console.log(`Listening at port ${process.env.PORT || port}`))
 
 
